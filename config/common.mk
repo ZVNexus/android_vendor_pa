b7e61122efaa32f5a29e79f7baebe945f6fc310e
Alexander Koskovich <zvnexus@outlook.com>
2020-07-22 01:20:20 -0400

    Inherit from Pixel vendor.
     * Moved Pixel specific features out of core GMS package.
    
    Change-Id: Ibc2e67a76afe9d4d59b971b871aca6f1d9ffa02a

--- config/common.mk
+++ config/common.mk
@@ -56,6 +56,7 @@ ifneq ($(TARGET_DISABLES_GAPPS), true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
 $(call inherit-product-if-exists, vendor/partner_gms/products/turbo.mk)
 $(call inherit-product-if-exists, vendor/google/gms/gms-vendor.mk)
+$(call inherit-product-if-exists, vendor/google/pixel/pixel-vendor.mk)
 
 # Do not preoptimize prebuilts when building GApps
 DONT_DEXPREOPT_PREBUILTS := true

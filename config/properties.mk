f4bf56e325f081283097f14c81d4d97d295a3e57
Alexander Koskovich <zvnexus@outlook.com>
2020-08-06 18:34:24 -0400

    Revert "pa: config: Don't bother overriding fingerprint."
    This reverts commit 6dc26e77fddb6d4b72e352fba4e409291c08278d.

--- config/properties.mk
+++ config/properties.mk
@@ -25,6 +25,11 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
+# Fingerprint
+ifeq ($(TARGET_BUILD_VARIANT),user)
+BUILD_FINGERPRINT ?= google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
+endif
+
 # Gestures
 ifneq ($(TARGET_USES_HARDWARE_KEYS),true)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
